e8c4dcc17f0cd6a8b37550b04bf802625ae7b36a
Денис Фролов <denffv@yandex.ru>
2023-01-31 15:55:04 +0500

    fix api

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -23,7 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework_simplejwt',
     'reviews.apps.ReviewsConfig',
-    'api',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
