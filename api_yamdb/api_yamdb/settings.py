c0b7ecb84eed1ee9099083bc32d66404d5fbf904
Денис Фролов <denffv@yandex.ru>
2023-01-31 11:15:12 +0500

    jwt, auth in settings, app api, api/urls token, path redoc

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -22,7 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework_simplejwt',
-    'reviews.apps.ReviewsConfig',
+    'reviews.apps.ReviewsConfig',?
     'api',
 ]
 
