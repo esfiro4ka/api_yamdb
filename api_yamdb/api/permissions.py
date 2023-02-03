c311291bfd6ed852bc111c402a063d946030ffec
Ольга Ротбардт <esfiro4ka@yandex.ru>
2023-02-03 05:31:12 +0400

    Permissions: атрибуты is_admin и др

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -4,27 +4,23 @@ from rest_framework import permissions
 class IsAdminOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        return (request.method in permissions.SAFE_METHODS or (
-                request.user.is_authenticated
-                and (request.user.is_admin or request.user.is_superuser)))
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user.is_admin
 
 
-class IsAdminModeratorOrReadOnly(permissions.BasePermission):
+class IsAdminModeratorAuthorOrReadOnly(permissions.BasePermission):
+    """
+    Разрешение для авторов, модераторов и администраторов
+    на редактирование и удаление объектов.
+    """
 
     def has_object_permission(self, request, view, obj):
-        return (request.method in permissions.SAFE_METHODS
-                or request.user.is_admin
-                or request.user.is_moderator
-                or obj.author == request.user)
-
-    def has_permission(self, request, view):
-        return (request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated)
-
-
-class IsAdmin(permissions.BasePermission):
-
-    def has_permission(self, request, view):
-        return (request.user.is_authenticated
-                and request.user.is_admin
-                or request.user.is_superuser)
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        elif request.method == 'POST' and request.user.is_user:
+            return True
+        return request.method in ['PATCH',
+                                  'DELETE'] and (request.user.is_admin
+                                                 or request.user.is_moderator
+                                                 or request.user == obj.author)
