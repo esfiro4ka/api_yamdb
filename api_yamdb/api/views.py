aa8c817f4dc4251d7dbc2669f315962b9ebc7c40
Polina Scherbakova <tiessearch@yandex.ru>
2023-02-03 18:33:54 +0400

    Add Title's filter, add management-command

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404
+from django.db.models import Avg
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, filters
 # from django.shortcuts import render
@@ -7,6 +8,7 @@ from api.serializers import (ReviewSerializer, TitleSerializer,
                              GenreSerializer, CommentSerializer,
                              UserSerializer)
 from reviews.models import Title, Category, Genre, Review, User
+from api.filters import TitlesFilter
 from api.mixins import CreateListDestroyViewSet
 from api.permissions import IsAdminOrReadOnly, IsAdminModeratorAuthorOrReadOnly
 from rest_framework import generics
@@ -79,9 +81,12 @@ class GenreViewSet(CreateListDestroyViewSet):
 
 class TitleViewSet(viewsets.ModelViewSet):
     """Получение списка всех произведений."""
-    queryset = Title.objects.all()
+    queryset = Title.objects.all()#.annotate(
+        #avg_rating=Avg('review__rating')
+        #).order_by('-avg_rating')
     serializer_class = TitleSerializer
     filter_backends = (DjangoFilterBackend,)
+    filterset_class = TitlesFilter
     permission_classes = (IsAuthenticatedOrReadOnly,)
 
 
